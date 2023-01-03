1a3d13b4d2d7881bf255de3614b9927b82225528
avshmygol <avshmygol@yandex.ru>
2023-01-03 18:37:11 +0300

    Added devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) { |u|
+      u.permit(:password, :password_confirmation, :current_password)
+    }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
   has_many :events
 
   private
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 35 }
   validates :name, format: { with: /\A[0-9a-z_]+\z/i }
-  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
-  validates :email, format: { with: /\A[a-z0-9\-_.]+@[a-z0-9\-_.]+\z/i }
 end
