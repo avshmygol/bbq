c6a0fd0eacf698a70961e28b308d34bce74aa65b
Woodpecker2020 <927456@gmail.com>
2023-01-17 22:24:22 +0300

    Update subscription actions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -10,15 +10,24 @@ class SubscriptionsController < ApplicationController
     @new_subscription = @event.subscriptions.build(subscription_params)
     @new_subscription.user = current_user
 
-    if @new_subscription.save
-      # Отправляем письмо автору события о подписке
-      EventMailer.subscription(@event, @new_subscription).deliver_now
-
-      # Если сохранилась успешно, редирект на страницу самого события
-      redirect_to @event, notice: I18n.t('controllers.subscriptions.created')
+    # Запрещаем анонимным пользователям использовать чужую почту
+    if @new_subscription.user.nil? && User.find_by(email: @new_subscription.user_email)
+      # Запишем основные ошибки валидации (имя, почта)
+      @new_subscription.valid?
+      # Добавим ошибку использования чужой почты
+      @new_subscription.errors.add(:base, I18n.t('controllers.subscriptions.error_email'))
+      render 'events/show'
     else
-      # если ошибки — рендерим здесь же шаблон события
-      render 'events/show', alert: I18n.t('controllers.subscriptions.error')
+      if @event.user != current_user && @new_subscription.save
+        # Отправляем письмо автору события о подписке
+        # EventMailer.subscription(@event, @new_subscription).deliver_now
+
+        # Перерисовываем окно мероприятия
+        redirect_to @event, notice: I18n.t('controllers.subscriptions.created')
+      else
+        # Перерисовываем форму ввода с указанием ошибок валидации
+        render 'events/show'
+      end
     end
   end
 
@@ -27,7 +36,7 @@ class SubscriptionsController < ApplicationController
 
     if current_user_can_edit?(@subscription)
       # Отправляем письмо автору события об отписке
-      EventMailer.subscription_destroy(@event, @subscription).deliver_now
+      # EventMailer.subscription_destroy(@event, @subscription).deliver_now
 
       @subscription.destroy
     else
--- lib/tasks/001_initial_data.rake
+++ lib/tasks/001_initial_data.rake
@@ -8,7 +8,7 @@ namespace :setup do
     Rake::Task["db:migrate"].invoke
 
     puts "*** Add initial data ***"
-    lines = File.readlines("#{__dir__}/001_initial_data.txt", encoding: "UTF-8", chomp: true)
+    lines = File.readlines(Rails.root.join('lib', 'tasks', '001_initial_data.txt'), encoding: "UTF-8", chomp: true)
 
     # Пользователь 1
     user1 = User.new(
@@ -17,8 +17,26 @@ namespace :setup do
       password: lines[3],
       password_confirmation: lines[3]
     )
-
     user1.save
+
+    # Пользователь 2
+    user2 = User.new(
+      name: lines[4],
+      email: lines[5],
+      password: lines[6],
+      password_confirmation: lines[6]
+    )
+    user2.save
+
+    # Мероприятие 1
+    event = Event.new(
+      title: lines[8],
+      address: lines[9],
+      description: lines[10],
+      datetime: Time.now,
+      user_id: user1.id
+    )
+    event.save
 
   end
 end
