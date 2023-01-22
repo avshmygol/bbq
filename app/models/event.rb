91f5a4632120bf5c01fd50c54854e8e4186ed861
Woodpecker2020 <927456@gmail.com>
2023-01-22 11:30:15 +0300

    Update devise views

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,13 +1,13 @@
 class Event < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :address, presence: true
+  validates :datetime, presence: true
+
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
   has_many :subscribers, through: :subscriptions, source: :user
   has_many :photos, dependent: :destroy
-
-  validates :title, presence: true, length: { maximum: 255 }
-  validates :address, presence: true
-  validates :datetime, presence: true
 
   def visitors
     ([user] + subscribers).uniq
